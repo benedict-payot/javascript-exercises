4aad013990a8e517f64bfd24b1934c1497f4f4db
Benedict Payot <159107303+benedictpayot@users.noreply.github.com>
2024-08-04 18:51:54 +0800

    Edit repeatString function to pass the first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
 const repeatString = function() {
-
+    let str = "hey";
+    const result = [];
+    for(i=0; i<3; i++) {
+        result.push(str);
+    }
+    return result.join("");
+    
 };
 
 // Do not edit below this line

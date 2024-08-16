324f5e5192b09dbf97af051e3d5a955632d1de08
Benedict Payot <159107303+benedictpayot@users.noreply.github.com>
2024-08-16 21:02:24 +0800

    Create sumAll function to pass all the tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let numbers = [];
+    let sum = 0;
+    if(!Number.isInteger(min) || !Number.isInteger(max) || min < 0 || max < 0) { return "ERROR"; }
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+      }
 
+    for(i=min; i<=max; i++) {
+        numbers.push(i);
+    }
+    
+    numbers.forEach(num => {
+        sum += num;
+    });
+
+    return sum;
 };
 
 // Do not edit below this line

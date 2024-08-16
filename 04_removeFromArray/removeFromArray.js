837da7afa80f4c2267ecc48ff20f5fd3391123b7
Benedict Payot <159107303+benedictpayot@users.noreply.github.com>
2024-08-16 19:27:24 +0800

    Create removeFromArray function to pass all the tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,21 +1,3 @@
-// const removeFromArray = function(array, ...args) {
-//     // if(haystack.includes(needle)) {
-//     //     needleIndex = haystack.indexOf(needle); 
-//     //     if(needle.length>1) {
-//     //         cleanHaystack = haystack.splice(needleIndex, needle.length);
-           
-//     //     }
-//     //     else {
-//     //         cleanHaystack = haystack.splice(needleIndex, 1);
-//     //     }
-//     //     console.log(needleIndex);
-//     //     // return haystack;
-//     // }
-
-   
-
-// };
-
 const removeFromArray = function(array, ...args) {
     return array.filter(val => !args.includes(val));
 }

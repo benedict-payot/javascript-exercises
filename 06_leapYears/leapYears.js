f532acac44b21f66ab9ce4bd36ade98a51e65858
Benedict Payot <159107303+benedictpayot@users.noreply.github.com>
2024-08-16 22:08:15 +0800

    Create leapYears function to pass all the tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const isbyFour = year % 4 === 0;
+    const isHundred = year % 100 === 0;
+    const isHundredByFour = year % 400 === 0;
+    if(isbyFour && (!isHundred || isHundredByFour)) {
+        return true;
+    }
+   else {
+        return false;
+   }
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
